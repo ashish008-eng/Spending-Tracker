8eb738c8dfdd0e3f925736f545fb7ebddb20357b
ashish008-eng <afitness008@gmail.com>
2026-04-15 21:42:17 +0530

    Fixed CORS for production

--- src/main/java/com/example/spendingTracker/config/SecurityConfig.java
+++ src/main/java/com/example/spendingTracker/config/SecurityConfig.java
@@ -120,10 +120,11 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500"
-        ));
+//        config.setAllowedOrigins(List.of(
+//                "http://127.0.0.1:5500",
+//                "http://localhost:5500"
+//        ));
+        config.setAllowedOriginPatterns(List.of("*"));
 
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
